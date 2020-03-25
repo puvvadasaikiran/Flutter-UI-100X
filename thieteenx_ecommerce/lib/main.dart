55c433582b0956e90149cd1bd09f5a737c5f5b95
puvvadasaikiran <30699361+puvvadasaikiran@users.noreply.github.com>
2020-03-25 13:34:19 +0530

    ThirteenX intial Commit

---
+++ thieteenx_ecommerce/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
